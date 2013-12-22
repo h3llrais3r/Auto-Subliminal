459a7576239b93249350771ee21c54924df42f95
h3llrais3r <pooh_beer_1@hotmail.com>
2013-12-22 14:57:17 +0100

    Added setup script
    Add setup script to install AutoSubliminal and required modules

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(name='Auto-Subliminal',
+    version='0.5.9-dev',
+    license='GPL v3',
+    description='Auto-Subliminal, the automated subliminal subtitle downloader',
+    long_description=open('README.md').read(),
+    keywords='subtitle subtitles video movie episode tv show',
+    url='https://github.com/h3llrais3r/Auto-Subliminal',
+    author='h3llrais3r',
+    author_email='pooh_beer_1@hotmail.com',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').readlines())
