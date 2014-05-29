dec485eff8637f6d0a8a1b6d245c1eb4a2566249
h3llrais3r <pooh_beer_1@hotmail.com>
2014-05-29 21:36:52 +0200

    Added encoding to postprocessor
    This is needed is you want to handle postprocessing commands and
    arguments with non ascii characters

--- autosubliminal/postprocessor.py
+++ autosubliminal/postprocessor.py
@@ -8,13 +8,14 @@ log = logging.getLogger(__name__)
 class PostProcessor():
     """
     Post Processor. It launches the specified command and retrieves the arguments from the download_dict.
-    Additionally, some optional arguments can be passed.
+    Additionally, some optional arguments and the encoding (for command and arguments) can be passed.
     """
 
-    def __init__(self, cmd, download_dict, args=None):
+    def __init__(self, cmd, download_dict, args=None, encoding=None):
         self.cmd = cmd
-        self.download_dict = download_dict
         self.args = args
+        self.encoding = encoding
+        self.download_dict = download_dict
 
     def run(self):
         process_cmd = self._constuct_process_cmd()
@@ -28,19 +29,30 @@ class PostProcessor():
         log.info("Running postprocessor:")
         log.info("Command:")
         log.info("%s" % self.cmd)
-        process = [self.cmd]
+        process = [self._encode(self.cmd)]
         log.info("Arguments:")
 
-        # Add the default arguments from the download_dict
-        process.append(self.download_dict['destinationFileLocationOnDisk'])
-        log.info("%s" % self.download_dict['destinationFileLocationOnDisk'])
-        process.append(self.download_dict['originalFileLocationOnDisk'])
-        log.info("%s" % self.download_dict['originalFileLocationOnDisk'])
+        # Add the encoding
+        log.info("%s" % self.encoding)
+        process.append(self.encoding)
 
-        # Check for additional arguments
+        # Add the default arguments (episode and subtitle) from the download_dict
+        episode = self.download_dict['originalFileLocationOnDisk']
+        subtitle = self.download_dict['destinationFileLocationOnDisk']
+        log.info("%s" % episode)
+        process.append(self._encode(episode))
+        log.info("%s" % subtitle)
+        process.append(self._encode(subtitle))
+
+        # Add additional arguments if needed
         if self.args:
             for arg in self.args:
                 log.info("%s" % arg)
-                process.append(arg)
+                process.append(self._encode(arg))
         log.info("#" * 30)
         return process
+
+    def _encode(self, value):
+        if self.encoding:
+            return value.encode(self.encoding)
+        return value
