e57c4277fca1b88781d4d39ddb7bf57c1701e09b
h3llrais3r <pooh_beer_1@hotmail.com>
2014-06-12 20:34:37 +0200

    Renamed scheduler command to process

--- autosubliminal/scheduler.py
+++ autosubliminal/scheduler.py
@@ -5,27 +5,27 @@ import traceback
 
 
 class Scheduler:
-    def __init__(self, command, interval, runnow, name):
+    def __init__(self, process, interval, runnow, name):
         self.lastrun = time.time()
-        self.command = command
+        self.process = process
         self.name = name
         self.interval = interval
-        self.thread = threading.Thread(None, self.runcommand, self.name)
+        self.thread = threading.Thread(None, self.run_process, self.name)
         self.stop = False
         if runnow:
             try:
-                self.command.run()
+                self.process.run()
                 self.runnow = False
             except:
                 print traceback.format_exc()
                 os._exit(1)
 
-    def runcommand(self):
+    def run_process(self):
         while True:
             currentime = time.time()
             if currentime - self.lastrun > self.interval:
                 try:
-                    if self.command.run():
+                    if self.process.run():
                         self.lastrun = currentime
                 except:
                     print traceback.format_exc()
@@ -33,7 +33,7 @@ class Scheduler:
 
             if self.runnow:
                 try:
-                    if self.command.run():
+                    if self.process.run():
                         self.lastrun = currentime
                         self.runnow = False
                 except:
