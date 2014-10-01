bfad3bdc5589765a241a9070b76916c64b9fac5e
h3llrais3r <pooh_beer_1@hotmail.com>
2014-10-01 22:02:41 +0200

    Prepared version 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='Auto-Subliminal',
-    version='0.6.0',
+    version='1.0.0',
     license='GPL v3',
     description='Auto-Subliminal, the automated subliminal subtitle downloader',
     long_description=open('README.md').read(),
