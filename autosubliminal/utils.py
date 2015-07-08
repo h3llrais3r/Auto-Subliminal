dc21cd052af3f68e0e00c49e667990e66f344838
h3llrais3r <pooh_beer_1@hotmail.com>
2015-07-08 08:59:21 +0200

    Show file size of video in info dropdown

--- autosubliminal/utils.py
+++ autosubliminal/utils.py
@@ -352,6 +352,11 @@ def release_wanted_queue_lock():
         log.warning("Trying to release a wanted queue lock while there is no lock")
 
 
+def get_file_size(path):
+    byte_size = os.path.getsize(path)
+    return humanize_bytes(byte_size, 2)
+
+
 # Thanks to: http://stackoverflow.com/questions/1088392/sorting-a-python-list-by-key-while-checking-for-string-or-float
 def get_attr(name):
     def inner_func(o):
@@ -362,3 +367,41 @@ def get_attr(name):
         return rv
 
     return inner_func
+
+
+# Thanks to: http://code.activestate.com/recipes/577081-humanized-representation-of-a-number-of-bytes/
+def humanize_bytes(bytes, precision=1):
+    """
+    Return a humanized string representation of a number of bytes.
+
+    >>> humanize_bytes(1)
+    '1 byte'
+    >>> humanize_bytes(1024)
+    '1.0 kB'
+    >>> humanize_bytes(1024*123)
+    '123.0 kB'
+    >>> humanize_bytes(1024*12342)
+    '12.1 MB'
+    >>> humanize_bytes(1024*12342,2)
+    '12.05 MB'
+    >>> humanize_bytes(1024*1234,2)
+    '1.21 MB'
+    >>> humanize_bytes(1024*1234*1111,2)
+    '1.31 GB'
+    >>> humanize_bytes(1024*1234*1111,1)
+    '1.3 GB'
+    """
+    abbrevs = (
+        (1 << 50L, 'PB'),
+        (1 << 40L, 'TB'),
+        (1 << 30L, 'GB'),
+        (1 << 20L, 'MB'),
+        (1 << 10L, 'kB'),
+        (1, 'bytes')
+    )
+    if bytes == 1:
+        return '1 byte'
+    for factor, suffix in abbrevs:
+        if bytes >= factor:
+            break
+    return '%.*f %s' % (precision, bytes / factor, suffix)
