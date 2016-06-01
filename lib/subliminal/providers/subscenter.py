de8c0c6ce5d14616bc78c99a43cda84c9ec5e6f1
h3llrais3r <pooh_beer_1@hotmail.com>
2016-06-01 19:46:10 +0200

    Upgrade subliminal to version 2.0-rc1 Use develop branch, commit e2331bef2c827406f92b1696b96241137163fe4f

--- lib/subliminal/providers/subscenter.py
+++ lib/subliminal/providers/subscenter.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 import bisect
+from collections import defaultdict
 import io
 import json
 import logging
 import zipfile
 
 from babelfish import Language
-from guessit import guess_episode_info, guess_movie_info
+from guessit import guessit
 from requests import Session
 
-from . import ParserBeautifulSoup, Provider, get_version
-from .. import __version__
+from . import ParserBeautifulSoup, Provider
+from .. import __short_version__
 from ..cache import SHOW_EXPIRATION_TIME, region
 from ..exceptions import AuthenticationError, ConfigurationError, ProviderError
-from ..subtitle import Subtitle, fix_line_ending, guess_matches, sanitized_string_equal
+from ..subtitle import Subtitle, fix_line_ending, guess_matches
+from ..utils import sanitize
 from ..video import Episode, Movie
 
 logger = logging.getLogger(__name__)
 
 
 class SubsCenterSubtitle(Subtitle):
+    """SubsCenter Subtitle."""
     provider_name = 'subscenter'
 
     def __init__(self, language, hearing_impaired, page_link, series, season, episode, title, subtitle_id, subtitle_key,
@@ -38,13 +41,13 @@ class SubsCenterSubtitle(Subtitle):
     def id(self):
         return str(self.subtitle_id)
 
-    def get_matches(self, video, hearing_impaired=False):
-        matches = super(SubsCenterSubtitle, self).get_matches(video, hearing_impaired=hearing_impaired)
+    def get_matches(self, video):
+        matches = set()
 
         # episode
         if isinstance(video, Episode):
             # series
-            if video.series and sanitized_string_equal(self.series, video.series):
+            if video.series and sanitize(self.series) == sanitize(video.series):
                 matches.add('series')
             # season
             if video.season and self.season == video.season:
@@ -54,23 +57,24 @@ class SubsCenterSubtitle(Subtitle):
                 matches.add('episode')
             # guess
             for release in self.releases:
-                matches |= guess_matches(video, guess_episode_info(release + '.mkv'))
+                matches |= guess_matches(video, guessit(release, {'type': 'episode'}))
         # movie
         elif isinstance(video, Movie):
             # guess
             for release in self.releases:
-                matches |= guess_matches(video, guess_movie_info(release + '.mkv'))
+                matches |= guess_matches(video, guessit(release, {'type': 'movie'}))
 
         # title
-        if video.title and sanitized_string_equal(self.title, video.title):
+        if video.title and sanitize(self.title) == sanitize(video.title):
             matches.add('title')
 
         return matches
 
 
 class SubsCenterProvider(Provider):
+    """SubsCenter Provider."""
     languages = {Language.fromalpha2(l) for l in ['he']}
-    server = 'http://subscenter.cinemast.com/he/'
+    server_url = 'http://subscenter.cinemast.com/he/'
 
     def __init__(self, username=None, password=None):
         if username is not None and password is None or username is None and password is not None:
@@ -82,12 +86,12 @@ class SubsCenterProvider(Provider):
 
     def initialize(self):
         self.session = Session()
-        self.session.headers = {'User-Agent': 'Subliminal/%s' % get_version(__version__)}
+        self.session.headers['User-Agent'] = 'Subliminal/%s' % __short_version__
 
         # login
         if self.username is not None and self.password is not None:
             logger.debug('Logging in')
-            url = self.server + 'subscenter/accounts/login/'
+            url = self.server_url + 'subscenter/accounts/login/'
 
             # retrieve CSRF token
             self.session.get(url)
@@ -107,7 +111,7 @@ class SubsCenterProvider(Provider):
         # logout
         if self.logged_in:
             logger.info('Logging out')
-            r = self.session.get(self.server + 'subscenter/accounts/logout/', timeout=10)
+            r = self.session.get(self.server_url + 'subscenter/accounts/logout/', timeout=10)
             r.raise_for_status()
             logger.info('Logged out')
             self.logged_in = False
@@ -115,53 +119,51 @@ class SubsCenterProvider(Provider):
         self.session.close()
 
     @region.cache_on_arguments(expiration_time=SHOW_EXPIRATION_TIME)
-    def _search_url_title(self, title, kind):
-        """Search the URL title for the given `title`.
+    def _search_url_titles(self, title):
+        """Search the URL titles by kind for the given `title`.
 
         :param str title: title to search for.
-        :param str kind: kind of the title, ``movie`` or ``series``.
-        :return: the URL version of the title.
-        :rtype: str or None
+        :return: the URL titles by kind.
+        :rtype: collections.defaultdict
 
         """
         # make the search
         logger.info('Searching title name for %r', title)
-        r = self.session.get(self.server + 'subtitle/search/', params={'q': title}, allow_redirects=False, timeout=10)
+        r = self.session.get(self.server_url + 'subtitle/search/', params={'q': title}, timeout=10)
         r.raise_for_status()
 
-        # if redirected, get the url title from the Location header
-        if r.is_redirect:
-            parts = r.headers['Location'].split('/')
-
-            # check kind
-            if parts[-3] == kind:
-                return parts[-2]
-
-            return None
-
-        # otherwise, get the first valid suggestion
+        # get the suggestions
         soup = ParserBeautifulSoup(r.content, ['lxml', 'html.parser'])
-        suggestions = soup.select('#processes div.generalWindowTop a')
-        logger.debug('Found %d suggestions', len(suggestions))
-        for suggestion in suggestions:
-            parts = suggestion.attrs['href'].split('/')
+        links = soup.select('#processes div.generalWindowTop a')
+        logger.debug('Found %d suggestions', len(links))
+        url_titles = defaultdict(list)
+        for link in links:
+            parts = link.attrs['href'].split('/')
+            url_titles[parts[-3]].append(parts[-2])
 
-            # check kind
-            if parts[-3] == kind:
-                return parts[-2]
+        return url_titles
 
-    def query(self, series=None, season=None, episode=None, title=None):
-        # set the correct parameters depending on the kind
-        if series and season and episode:
-            url_series = self._search_url_title(series, 'series')
-            url = self.server + 'cinemast/data/series/sb/{}/{}/{}/'.format(url_series, season, episode)
-            page_link = self.server + 'subtitle/series/{}/{}/{}/'.format(url_series, season, episode)
-        elif title:
-            url_title = self._search_url_title(title, 'movie')
-            url = self.server + 'cinemast/data/movie/sb/{}/'.format(url_title)
-            page_link = self.server + 'subtitle/movie/{}/'.format(url_title)
+    def query(self, title, season=None, episode=None):
+        # search for the url title
+        url_titles = self._search_url_titles(title)
+
+        # episode
+        if season and episode:
+            if 'series' not in url_titles:
+                logger.error('No URL title found for series %r', title)
+                return []
+            url_title = url_titles['series'][0]
+            logger.debug('Using series title %r', url_title)
+            url = self.server_url + 'cinemast/data/series/sb/{}/{}/{}/'.format(url_title, season, episode)
+            page_link = self.server_url + 'subtitle/series/{}/{}/{}/'.format(url_title, season, episode)
         else:
-            raise ValueError('One or more parameters are missing')
+            if 'movie' not in url_titles:
+                logger.error('No URL title found for movie %r', title)
+                return []
+            url_title = url_titles['movie'][0]
+            logger.debug('Using movie title %r', url_title)
+            url = self.server_url + 'cinemast/data/movie/sb/{}/'.format(url_title)
+            page_link = self.server_url + 'subtitle/movie/{}/'.format(url_title)
 
         # get the list of subtitles
         logger.debug('Getting the list of subtitles')
@@ -191,7 +193,7 @@ class SubsCenterProvider(Provider):
                             continue
 
                         # otherwise create it
-                        subtitle = SubsCenterSubtitle(language, hearing_impaired, page_link, series, season, episode,
+                        subtitle = SubsCenterSubtitle(language, hearing_impaired, page_link, title, season, episode,
                                                       title, subtitle_id, subtitle_key, downloaded, [release])
                         logger.debug('Found subtitle %r', subtitle)
                         subtitles[subtitle_id] = subtitle
@@ -199,21 +201,19 @@ class SubsCenterProvider(Provider):
         return subtitles.values()
 
     def list_subtitles(self, video, languages):
-        series = None
-        season = None
-        episode = None
+        season = episode = None
         title = video.title
 
         if isinstance(video, Episode):
-            series = video.series
+            title = video.series
             season = video.season
             episode = video.episode
 
-        return [s for s in self.query(series, season, episode, title) if s.language in languages]
+        return [s for s in self.query(title, season, episode) if s.language in languages]
 
     def download_subtitle(self, subtitle):
         # download
-        url = self.server + 'subtitle/download/{}/{}/'.format(subtitle.language.alpha2, subtitle.subtitle_id)
+        url = self.server_url + 'subtitle/download/{}/{}/'.format(subtitle.language.alpha2, subtitle.subtitle_id)
         params = {'v': subtitle.releases[0], 'key': subtitle.subtitle_key}
         r = self.session.get(url, params=params, headers={'Referer': subtitle.page_link}, timeout=10)
         r.raise_for_status()
