4c61cd13d82d6de148d09dada8ad0df486c12876
h3llrais3r <pooh_beer_1@hotmail.com>
2016-06-01 21:31:56 +0200

    Fix test requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import sys
 from setuptools import setup, find_packages
 
+
+# integrated libraries - add them to the system path (needed for running tests)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
 
 # requirements
 setup_requirements = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
 
 install_requirements = open('requirements.txt').readlines()
 
-test_requirements = ['pytest'] + open('libraries.txt').readlines()
+test_requirements = ['pytest']
 
 dev_requirements = []
 
+# setup
 setup(name='Auto-Subliminal',
       version='2.1.1',
       license='GPL v3',
