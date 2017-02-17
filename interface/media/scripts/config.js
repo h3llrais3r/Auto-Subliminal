8cc20445ecdb3e52ec01fadd6f3fe2f25be4c946
h3llrais3r <pooh_beer_1@hotmail.com>
2017-02-17 17:39:13 +0100

    Delete unused config.js

--- interface/media/scripts/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Javascript needed on the config page
- */
-
-// Activate the configtabs
-$("#configtabs a").click(function (event) {
-    event.preventDefault();
-    $(this).tab("show");
-});
