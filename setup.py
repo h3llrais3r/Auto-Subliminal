fe00231fd26c44dcfaaa5c530a5622d85e944138
h3llrais3r <pooh_beer_1@hotmail.com>
2017-12-12 21:38:15 +0100

    Read version from version.py in setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,24 @@
 # -*- coding: utf-8 -*-
 import io
 import os
+import re
 import sys
 from setuptools import setup, find_packages
-
 
 # integrated libraries - add them to the system path (needed for running tests)
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
 
 # root path
 root_path = os.path.abspath(os.path.dirname(__file__))
+
+# version (version must be compliant with version.StrictVersion of distutils)
+with io.open(os.path.join(root_path, 'autosubliminal', 'version.py'), 'r') as f:
+    version = re.search(r'^RELEASE_VERSION\s*=\s*[\'\"]((\d+)\.(\d+)(\.(\d+))?([ab](\d+))?)[\'\"]$', f.read(),
+                        re.MULTILINE).group(1)
+
+# readme
+with io.open(os.path.join(root_path, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
 
 # requirements
 setup_requirements = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
@@ -21,18 +30,12 @@ test_requirements = ['pytest', 'pytest-pep8', 'pytest-flakes']
 
 dev_requirements = ['Cheetah', 'tox']
 
-
-# read function
-def read(*paths):
-    return io.open(os.path.join(root_path, *paths), 'r').read()
-
-
 # setup
 setup(name='Auto-Subliminal',
-      version='2.2.1',
+      version=version,
       license='GPL v3',
       description='Auto-Subliminal, the automated subliminal subtitle downloader',
-      long_description=read('README.md'),
+      long_description=readme,
       keywords='subtitle subtitles video movie episode tv show',
       url='https://github.com/h3llrais3r/Auto-Subliminal',
       author='h3llrais3r',
