27733e1c953ddaefb8cc4c0d24061e419258dc2f
h3llrais3r <pooh_beer_1@hotmail.com>
2018-02-23 23:29:26 +0100

    Try to fix build

--- setup.py
+++ setup.py
@@ -6,22 +6,35 @@ import re
 import sys
 from setuptools import setup, find_packages
 
-# integrated libraries - add them to the system path (needed for running tests)
+# Integrated libraries - add them to the system path (needed for running tests)
+# Insert libs that are only needed for certain python versions
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+PY34 = sys.version_info[0:2] >= (3, 4)
+if PY34:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'libpy34')))
+elif PY3:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'libpy3')))
+elif PY2:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'libpy2')))
+else:
+    pass
+# Insert common libs for all python versions
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
 
-# root path
+# Root path
 root_path = os.path.abspath(os.path.dirname(__file__))
 
-# version (version must be compliant with version.StrictVersion of distutils)
+# Version (version must be compliant with version.StrictVersion of distutils)
 with io.open(os.path.join(root_path, 'autosubliminal', 'version.py'), 'r') as f:
     version = re.search(r'^RELEASE_VERSION\s*=\s*[\'\"]((\d+)\.(\d+)(\.(\d+))?([ab](\d+))?)[\'\"]$', f.read(),
                         re.MULTILINE).group(1)
 
-# readme
+# Readme
 with io.open(os.path.join(root_path, 'README.md'), encoding='utf-8') as f:
     readme = f.read()
 
-# requirements
+# Requirements
 setup_requirements = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
 
 install_requirements = []
@@ -30,7 +43,7 @@ test_requirements = ['pytest', 'pytest-mock', 'pytest-pep8', 'pytest-flakes', 'r
 
 dev_requirements = ['tox']
 
-# setup
+# Setup
 setup(name='Auto-Subliminal',
       version=version,
       license='GPL v3',
