7b856cd31452d6f41de04d9fac4294ad6c72072b
h3llrais3r <pooh_beer_1@hotmail.com>
2018-02-24 09:52:35 +0100

    Make test_nma python 3 compatible
    Mocking response content should always return binary data.

--- tests/notifiers/test_nma.py
+++ tests/notifiers/test_nma.py
@@ -32,13 +32,13 @@ def test_nma_exception(monkeypatch):
 def test_nma_notify_download_error_response(monkeypatch):
     monkeypatch.setattr('autosubliminal.NOTIFYNMA', True)
     with requests_mock.mock() as m:
-        # Mock successful request
+        # Mock successful request (content must be binary data)
         m.register_uri('POST', NMAURL,
                        status_code=200,
-                       content='<?xml version="1.0" encoding="UTF-8"?>'
-                               '<nma>'
-                               '<error code="402" resettimer="15">Your IP exceeded...</error>'
-                               '</nma>')
+                       content=b'<?xml version="1.0" encoding="UTF-8"?>'
+                               b'<nma>'
+                               b'<error code="402" resettimer="15">Your IP exceeded...</error>'
+                               b'</nma>')
         notifier = NmaNotifier()
         assert notifier.name == notifier_name
         assert notifier.notify_download(**item_dict) is False
@@ -47,13 +47,13 @@ def test_nma_notify_download_error_response(monkeypatch):
 def test_nma_notify_download(monkeypatch):
     monkeypatch.setattr('autosubliminal.NOTIFYNMA', True)
     with requests_mock.mock() as m:
-        # Mock successful request
+        # Mock successful request (content must be binary data)
         m.register_uri('POST', NMAURL,
                        status_code=200,
-                       content='<?xml version="1.0" encoding="UTF-8"?>'
-                               '<nma>'
-                               '<success code="200" remaining="997" resettimer="52"/>'
-                               '</nma>')
+                       content=b'<?xml version="1.0" encoding="UTF-8"?>'
+                               b'<nma>'
+                               b'<success code="200" remaining="997" resettimer="52"/>'
+                               b'</nma>')
         notifier = NmaNotifier()
         assert notifier.name == notifier_name
         assert notifier.notify_download(**item_dict) is True
