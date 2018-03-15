8e9c63d86bd84ffcd288341047a16792316804b0
h3llrais3r <pooh_beer_1@hotmail.com>
2018-03-15 19:56:10 +0100

    Try to fix test_display_logfile on travis build

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -132,14 +132,15 @@ def test_mapping_string_to_dict():
 
 def test_display_logfile():
     try:
+        linesep = os.linesep
         line1 = u'2016-06-06 20:32:15,509 INFO     [MainThread :: __main__] Running application with PID: 9944'
         line2 = u'traceback'
         line3 = u'2016-06-06 20:32:15,509 DEBUG    [MainThread :: __main__] System encoding: cp1252'
         line4 = u'2016-06-06 20:32:15,509 DEBUG    [MainThread :: __main__] Config version: 10'
-        lines = line1 + '\r\n' + line2 + '\r\n' + line3 + '\r\n' + line4 + '\r\n'
-        lines_info = line1 + '\r\n' + line2 + '\r\n'
-        lines_debug = line3 + '\r\n' + line4 + '\r\n'
-        lines_debug_reversed = line4 + '\r\n' + line3 + '\r\n'
+        lines = line1 + linesep + line2 + linesep + line3 + linesep + line4 + linesep
+        lines_info = line1 + linesep + line2 + linesep
+        lines_debug = line3 + linesep + line4 + linesep
+        lines_debug_reversed = line4 + linesep + line3 + linesep
         fd, autosubliminal.LOGFILE = tempfile.mkstemp(text=True)
         file = open(autosubliminal.LOGFILE, 'w')
         file.write(line1 + '\n' + line2 + '\n' + line3 + '\n' + line4 + '\n')
