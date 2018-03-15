6e7a496a07b119d6258c78e063030577f61b464a
h3llrais3r <pooh_beer_1@hotmail.com>
2018-03-15 20:00:18 +0100

    Fix Python 3 build on travis

--- tests/test_wanteditem.py
+++ tests/test_wanteditem.py
@@ -8,30 +8,30 @@ wanted_item = WantedItem({'timestamp': '2018-01-01 12:30:01'})
 
 
 def test_is_search_active_before_on_creation(mocker):
-    today = datetime.datetime(2018, 01, 01, 0, 0, 0)
+    today = datetime.datetime(2018, 1, 1, 0, 0, 0)
     mocker.patch('autosubliminal.utils.today', return_value=today)
     assert wanted_item.search_active
 
 
 def test_is_search_active_before_deadline(mocker):
-    today = datetime.datetime(2018, 01, 02, 0, 0, 0)
+    today = datetime.datetime(2018, 1, 2, 0, 0, 0)
     mocker.patch('autosubliminal.utils.today', return_value=today)
     assert wanted_item.search_active
 
 
 def test_is_search_active_on_deadline(mocker):
-    today = datetime.datetime(2018, 01, 29, 0, 0, 0)
+    today = datetime.datetime(2018, 1, 29, 0, 0, 0)
     mocker.patch('autosubliminal.utils.today', return_value=today)
     assert wanted_item.search_active
 
 
 def test_is_search_active_after_deadline(mocker):
-    today = datetime.datetime(2018, 01, 30, 0, 0, 0)
+    today = datetime.datetime(2018, 1, 30, 0, 0, 0)
     mocker.patch('autosubliminal.utils.today', return_value=today)
     assert not wanted_item.search_active
 
 
 def test_is_search_active_after_deadline_on_delta(mocker):
-    today = datetime.datetime(2018, 02, 26, 0, 0, 0)
+    today = datetime.datetime(2018, 2, 26, 0, 0, 0)
     mocker.patch('autosubliminal.utils.today', return_value=today)
     assert wanted_item.search_active
