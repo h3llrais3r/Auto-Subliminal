9eaecf9764edfb03cedd6a69214495047497bd1d
h3llrais3r <pooh_beer_1@hotmail.com>
2018-03-27 20:37:33 +0200

    Try to fix test_api_wanted on travis

--- tests/server/api/test_api_wanted.py
+++ tests/server/api/test_api_wanted.py
@@ -6,25 +6,26 @@ import autosubliminal
 from autosubliminal.server.api.wanted import Wanted
 
 wanted_item = {
-    'videopath': 'Marvels.Agents.of.S.H.I.E.L.D.S05E03.720p.HDTV.x264-AVS[rarbg].mkv',
-    'season': 5,
-    'releasegrp': 'AVS',
-    'year': None,
-    'quality': '720p',
-    'episode': 3,
-    'tvdbid': 263365,
-    'title': 'Marvels Agents of S.H.I.E.L.D.',
-    'languages': [],
-    'source': 'HDTV',
-    'codec': 'h264',
-    'timestamp': u'2018-01-13 21:20:00',
-    'type': 'episode'
+    'codec': u'h264',
+    'episode': u'6',
+    'imdbid': None,
+    'languages': [u'nl'],
+    'quality': u'720p',
+    'releasegrp': u'NTb',
+    'season': u'1',
+    'source': u'WEB-DL',
+    'timestamp': u'2015-11-15 16:27:35',
+    'title': u'Mr Robot',
+    'type': u'episode',
+    'tvdbid': u'289590',
+    'videopath': u'Mr.Robot.S01E06.720p.WEB-DL.DD5.1.H.264-NTb.mkv',
+    'year': None
 }
 
-wanted_item_json = '{"episode": 3, "tvdbid": 263365, "title": "Marvels Agents of S.H.I.E.L.D.", "season": 5, ' \
-                   '"year": null, "releasegrp": "AVS", "languages": [], "source": "HDTV", "type": "episode", ' \
-                   '"codec": "h264", "timestamp": "2018-01-13 21:20:00", "quality": "720p", ' \
-                   '"videopath": "Marvels.Agents.of.S.H.I.E.L.D.S05E03.720p.HDTV.x264-AVS[rarbg].mkv"}'
+wanted_item_json = '{"codec": "h264", "episode": "6", "imdbid": null, "languages": ["nl"], "quality": "720p",' \
+                   ' "releasegrp": "NTb", "season": "1", "source": "WEB-DL", "timestamp": "2015-11-15 16:27:35",' \
+                   ' "title": "Mr Robot", "tvdbid": "289590", "type": "episode",' \
+                   ' "videopath": "Mr.Robot.S01E06.720p.WEB-DL.DD5.1.H.264-NTb.mkv", "year": null}'
 
 wanted_item_list_json = '[' + wanted_item_json + ']'
 
@@ -33,7 +34,10 @@ def test_get():
     # Create wanted queue with 1 item
     autosubliminal.WANTEDQUEUE = []
     autosubliminal.WANTEDQUEUE.append(wanted_item)
-    # Check conversion to json (need to pickle ourselves because we don't use cherrypy.tools here)
+    # Check conversion to json:
+    # - pickle ourselves because we don't use cherrypy.tools here
+    # - force sorted keys to be able to compare results (Python 3 sorts by default)
+    jsonpickle.set_encoder_options('simplejson', sort_keys=True)
     json_out = jsonpickle.encode(Wanted().get())
     assert wanted_item_list_json == json_out
 
