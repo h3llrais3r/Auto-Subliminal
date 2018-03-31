fec27bb17628c7cfe733d81db1270da19f6bfb29
h3llrais3r <pooh_beer_1@hotmail.com>
2018-03-31 22:06:22 +0200

    Add downloaded items to items api

--- autosubliminal/server/api/items.py
+++ autosubliminal/server/api/items.py
@@ -3,6 +3,7 @@
 import cherrypy
 
 import autosubliminal
+from autosubliminal.db import LastDownloads
 from autosubliminal.server.rest import RestResource
 
 
@@ -16,6 +17,7 @@ class ItemsApi(RestResource):
 
         # Add all sub paths here: /api/items/...
         self.wanted = _WantedApi()
+        self.downloaded = _DownloadedApi()
 
 
 @cherrypy.popargs('wanted_item_index')
@@ -48,3 +50,26 @@ class _WantedApi(RestResource):
         wanted_item = autosubliminal.WANTEDQUEUE.pop(int(wanted_item_index))
 
         return wanted_item
+
+
+@cherrypy.popargs('number_of_items')
+class _DownloadedApi(RestResource):
+    """
+    Rest resource for handling the /api/items/downloaded path.
+    """
+
+    def __init__(self):
+        super(_DownloadedApi, self).__init__()
+
+        # Set the allowed methods
+        self.allowed_methods = ('GET',)
+
+    def get(self, number_of_items=None):
+        """Get the list of downloaded items or the specified last number of downloaded items."""
+        last_downloads = LastDownloads().get_last_downloads()
+        if number_of_items is None:
+            return last_downloads
+        elif 0 <= int(number_of_items) <= len(last_downloads):
+            return last_downloads[0:int(number_of_items)]  # Return the requested number of items
+        else:
+            return self._bad_request('Invalid number_of_items')
