cc300af76efe9f73a38093c20456c9784627da54
h3llrais3r <pooh_beer_1@hotmail.com>
2018-03-31 21:23:30 +0200

    Fix json_out_handler for Python 3

--- autosubliminal/util/json.py
+++ autosubliminal/util/json.py
@@ -3,11 +3,15 @@
 import cherrypy
 import jsonpickle
 
+from autosubliminal.util.encoding import s2b
+
 
 def json_out_handler(*args, **kwargs):
     """
     Custom cherrypy json_handler which uses jsonpickle for converting objects to json.
     This is a copy from cherrypy.jsontools json_handler() but encodes with jsonpickle.
+
+    Remark: cherrypy requires a bytes output, so make sure to convert it to bytes.
     """
     value = cherrypy.serving.request._json_inner_handler(*args, **kwargs)
-    return jsonpickle.encode(value, unpicklable=False)
+    return s2b(jsonpickle.encode(value, unpicklable=False))
