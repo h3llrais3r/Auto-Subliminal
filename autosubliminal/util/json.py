7b91ea9afdff13f5dc744df097b20d3acb75f414
h3llrais3r <pooh_beer_1@hotmail.com>
2018-04-01 22:11:44 +0200

    Add from_json and to_json utility functions

--- autosubliminal/util/json.py
+++ autosubliminal/util/json.py
@@ -6,6 +6,26 @@ import jsonpickle
 from autosubliminal.util.encoding import s2b
 
 
+def from_json(value):
+    """
+    Convert json to an object. This will only work for objects which were converted to json with unpickable=True.
+    In all other cases, the result will be a dict instead.
+    :param value: The value to convert to an object
+    :return: The object or dict
+    """
+    return jsonpickle.loads(value)
+
+
+def to_json(value, unpickable=False):
+    """
+    Convert an object to json.
+    :param value: The value to convert to json
+    :param unpickable: If set to true, the json will contain extra info to convert the json back to the object
+    :return: The json string
+    """
+    return jsonpickle.dumps(value, unpicklable=unpickable)
+
+
 def json_out_handler(*args, **kwargs):
     """
     Custom cherrypy json_handler which uses jsonpickle for converting objects to json.
@@ -14,4 +34,4 @@ def json_out_handler(*args, **kwargs):
     Remark: cherrypy requires a bytes output, so make sure to convert it to bytes.
     """
     value = cherrypy.serving.request._json_inner_handler(*args, **kwargs)
-    return s2b(jsonpickle.encode(value, unpicklable=False))
+    return s2b(jsonpickle.dumps(value, unpicklable=False))
---
+++ tests/util/test_json.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+
+import jsonpickle
+
+from autosubliminal.util.json import from_json, to_json
+
+
+class MyClass(object):
+    def __init__(self, id):
+        self.id = id
+        self.key1 = 'key1'
+        self.key2 = 'key2'
+        self.object1 = MyChildClass(1, id)
+        self.object2 = MyChildClass(2, id)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+
+class MyChildClass(object):
+    def __init__(self, id, parent_id):
+        self.id = id + (parent_id * 10)
+        self.key1 = 'keychild1'
+        self.key2 = 'keychild2'
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+
+obj = MyClass(1)
+
+obj_dict = {'id': 1,
+            'key1': 'key1',
+            'key2': 'key2',
+            'object1': {'id': 11, 'key1': 'keychild1', 'key2': 'keychild2'},
+            'object2': {'id': 12, 'key1': 'keychild1', 'key2': 'keychild2'}
+            }
+
+obj_json = '{"id": 1,' \
+           ' "key1": "key1",' \
+           ' "key2": "key2",' \
+           ' "object1": {"id": 11, "key1": "keychild1", "key2": "keychild2"},' \
+           ' "object2": {"id": 12, "key1": "keychild1", "key2": "keychild2"}}'
+
+obj_json_unpickable_true = '{"id": 1,' \
+                           ' "key1": "key1",' \
+                           ' "key2": "key2",' \
+                           ' "object1": {"id": 11,' \
+                           ' "key1": "keychild1",' \
+                           ' "key2": "keychild2",' \
+                           ' "py/object": "test_json.MyChildClass"},' \
+                           ' "object2": {"id": 12,' \
+                           ' "key1": "keychild1",' \
+                           ' "key2": "keychild2",' \
+                           ' "py/object": "test_json.MyChildClass"},' \
+                           ' "py/object": "test_json.MyClass"}'
+
+# Force sorted keys to be able to compare results (Python 3 sorts by default)
+jsonpickle.set_encoder_options('simplejson', sort_keys=True)
+
+
+def test_object_from_json():
+    assert obj == from_json(obj_json_unpickable_true)
+
+
+def test_dict_from_json():
+    assert obj_dict == from_json(obj_json)
+
+
+def test_object_to_json():
+    assert obj_json == to_json(obj)
+
+
+def test_dict_to_json():
+    assert obj_json == to_json(obj_dict)
