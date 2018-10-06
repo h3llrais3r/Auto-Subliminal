ac1e9978c5a129df3efbd8b849f4fdc6de61b09f
h3llrais3r <pooh_beer_1@hotmail.com>
2018-10-06 18:06:43 +0200

    Fix test_queue.py

--- tests/util/test_queue.py
+++ tests/util/test_queue.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import autosubliminal
+from autosubliminal.core.item import WantedItem
 from autosubliminal.util.queue import count_wanted_queue_items, get_wanted_queue_lock, release_wanted_queue_lock
 
 
@@ -16,7 +17,9 @@ def test_wanted_queue_lock():
 
 
 def test_count_wanted_queue_items():
-    autosubliminal.WANTEDQUEUE = [{'type': 'movie', 'title': 'title1'}, {'type': 'episode', 'title': 'title2'}]
+    wanted_item_1 = WantedItem(type='movie', title='title1')
+    wanted_item_2 = WantedItem(type='episode', title='title2')
+    autosubliminal.WANTEDQUEUE = [wanted_item_1, wanted_item_2]
     assert count_wanted_queue_items() == 2
     assert count_wanted_queue_items(item_type='movie') == 1
     assert count_wanted_queue_items(item_type='episode') == 1
