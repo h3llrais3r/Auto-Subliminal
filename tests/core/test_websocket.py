bac6173568f41cb36d17960a813434a564e8ac86
h3llrais3r <pooh_beer_1@hotmail.com>
2018-11-07 20:20:59 +0100

    Update websocket tests

--- tests/core/test_websocket.py
+++ tests/core/test_websocket.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 
-from ws4py.messaging import TextMessage, BinaryMessage
+from ws4py.messaging import BinaryMessage, TextMessage
 
 import autosubliminal
-from autosubliminal.core.websocket import WebSocketHandler
+from autosubliminal.core.websocket import WebSocketBroadCaster, WebSocketHandler
 
 
 class MyProcess(object):
@@ -50,3 +50,19 @@ def test_handle_event_message_run_process_unsupported():
         }
     }
     assert not handler.handle_message(message)
+
+
+def test_websocket_broadcaster(monkeypatch, mocker):
+    mocker.patch('threading.Thread')
+    mocker.patch('cherrypy.engine.publish')
+    message = {
+        'message_type': 'event',
+        'event': {
+            'event_type': 'RUN_PROCESS',
+            'process': 'MyProcess'
+        }
+    }
+    monkeypatch.setattr('autosubliminal.WEBSOCKETMESSAGEQUEUE', [message])
+    broadcaster = WebSocketBroadCaster(name='WebSocketBroadCaster')
+    broadcaster.run()
+    assert len(autosubliminal.WEBSOCKETMESSAGEQUEUE) == 0
