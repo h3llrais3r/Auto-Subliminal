aebdee8e6f8e4f6c51e91c242afe2c414eb900a9
h3llrais3r <pooh_beer_1@hotmail.com>
2018-11-08 19:29:02 +0100

    Add cache test

---
+++ tests/core/test_cache.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import os
+import time
+
+from dogpile.cache.api import NoValue
+
+import autosubliminal
+from autosubliminal.core.cache import region, MutexFileLock
+
+cache_file = os.path.abspath(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'resources', 'test.cache.dbm'))
+
+
+def test_cache(monkeypatch):
+    monkeypatch.setattr('autosubliminal.core.cache.SCAN_VIDEO_EXPIRATION_TIME', 1)
+    region.configure(backend='dogpile.cache.dbm', expiration_time=autosubliminal.core.cache.SCAN_VIDEO_EXPIRATION_TIME,
+                     arguments={'filename': cache_file, 'lock_factory': MutexFileLock})
+    region.set('KEY', 'VALUE')
+    assert region.get('KEY') == 'VALUE'
+    time.sleep(2)
+    assert isinstance(region.get('KEY'), NoValue)
+    assert isinstance(region.get('UNKNOWN'), NoValue)
