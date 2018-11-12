7968e5d49d9574c84b42c45bbc28e4fe929f1ddd
h3llrais3r <pooh_beer_1@hotmail.com>
2018-11-12 20:04:11 +0100

    Rename TestRunner class to remove warning

--- tests/core/test_runner.py
+++ tests/core/test_runner.py
@@ -4,7 +4,7 @@ import time
 from autosubliminal.core.runner import Runner
 
 
-class TestRunner(Runner):
+class MyRunner(Runner):
     def run(self):
         pass
 
@@ -12,9 +12,9 @@ class TestRunner(Runner):
 def test_runner(mocker):
     # Use try/finally block to make sure that the thread is stopped
     try:
-        test_runner_mock = mocker.patch.object(TestRunner, 'run')
-        runner = TestRunner('TestRunner')
+        runner_mock = mocker.patch.object(MyRunner, 'run')
+        runner = MyRunner('MyRunner')
         time.sleep(2)  # Sleep to be sure that the run has been executed at least once
-        assert test_runner_mock.called
+        assert runner_mock.called
     finally:
         runner.stop()
