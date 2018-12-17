eaa100d3e1d4d10b6936243ec2b4425dae35e560
h3llrais3r <pooh_beer_1@hotmail.com>
2018-12-17 19:54:15 +0100

    Upgrade tvdb_api_v2 to version 0.3.2
    Install version at commit 7f02ecb9620017aed75973fd14d0197825c35140

--- lib/tvdb_api_v2/utils/artwork.py
+++ lib/tvdb_api_v2/utils/artwork.py
@@ -1,18 +1,26 @@
 # coding=utf-8
 
-BASE_ARTWORK_URL = "https://www.thetvdb.com"
+BASE_ARTWORK_URL = 'https://www.thetvdb.com'
 
 
-def get_artwork_url(artwork_name):
+def get_artwork_url(artwork_name, thumbnail=False):
     """Get the actual artwork url for download.
 
-    :param artwork_name: The artwork name
+    Returns the url of the full size artwork or the thumbnail version.
+    :param artwork_name: the artwork name
         This can be taken from:
             - The 'file_name' property in a SeriesImageQueryResult object from the get_series_images(...)
             - The 'thumbnail' property in a SeriesImageQueryResult object from the get_series_images(...)
             - the 'banner' property in a Series object from the SeriesData object from the get_series(...)
     :type artwork_name: str
-    :return: The full artwork url
+    :param thumbnail: the indication to return the thumbnail version or not
+    :type thumbnail: bool
+    :return: the full artwork url
     :rtype: str
     """
+    if thumbnail and '_cache' not in artwork_name:
+        # Thumbnail urls are the same as the normal url's but name is prefixed with '_cache/'
+        # Since we can also pass a thumbnail as artwork name, we need to make sure to not prefix it 2 times
+        return '{base_url}/banners/_cache/{artwork_name}'.format(base_url=BASE_ARTWORK_URL, artwork_name=artwork_name)
+
     return '{base_url}/banners/{artwork_name}'.format(base_url=BASE_ARTWORK_URL, artwork_name=artwork_name)
