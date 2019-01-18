9000d6eef187684bfce4f3eeb64febd004a51980
h3llrais3r <pooh_beer_1@hotmail.com>
2019-01-18 22:50:27 +0100

    Install soupsieve version 1.7.2

--- lib/soupsieve/util.py
+++ lib/soupsieve/util.py
@@ -4,8 +4,11 @@ from functools import wraps
 import warnings
 import sys
 import struct
+import os
+MODULE = os.path.dirname(__file__)
 
 PY3 = sys.version_info >= (3, 0)
+PY35 = sys.version_info >= (3, 5)
 
 if PY3:
     from functools import lru_cache  # noqa F401
@@ -26,75 +29,13 @@ else:
     unichar = unichr  # noqa
     string = basestring  # noqa
 
+_QUIRKS = 0x20000
 DEBUG = 0x10000
 
 LC_A = ord('a')
 LC_Z = ord('z')
 UC_A = ord('A')
 UC_Z = ord('Z')
-
-
-def is_doc(obj):
-    """Is `BeautifulSoup` object."""
-
-    import bs4
-    return isinstance(obj, bs4.BeautifulSoup)
-
-
-def is_tag(obj):
-    """Is tag."""
-
-    import bs4
-    return isinstance(obj, bs4.Tag)
-
-
-def is_comment(obj):
-    """Is comment."""
-
-    import bs4
-    return isinstance(obj, bs4.Comment)
-
-
-def is_declaration(obj):  # pragma: no cover
-    """Is declaration."""
-
-    import bs4
-    return isinstance(obj, bs4.Declaration)
-
-
-def is_cdata(obj):  # pragma: no cover
-    """Is CDATA."""
-
-    import bs4
-    return isinstance(obj, bs4.Declaration)
-
-
-def is_processing_instruction(obj):  # pragma: no cover
-    """Is processing instruction."""
-
-    import bs4
-    return isinstance(obj, bs4.ProcessingInstruction)
-
-
-def is_navigable_string(obj):
-    """Is navigable string."""
-
-    import bs4
-    return isinstance(obj, bs4.NavigableString)
-
-
-def is_special_string(obj):
-    """Is special string."""
-
-    import bs4
-    return isinstance(obj, (bs4.Comment, bs4.Declaration, bs4.CData, bs4.ProcessingInstruction))
-
-
-def get_navigable_string_type(obj):
-    """Get navigable string type."""
-
-    import bs4
-    return bs4.NavigableString
 
 
 def lower(string):
@@ -153,4 +94,40 @@ def warn_deprecated(message, stacklevel=2):  # pragma: no cover
         message,
         category=DeprecationWarning,
         stacklevel=stacklevel
+    )
+
+
+class QuirksWarning(UserWarning):  # pragma: no cover
+    """Warning for quirks mode."""
+
+
+def warn_quirks(message, recommend, pattern):
+    """Warn quirks."""
+
+    import traceback
+    import bs4  # noqa: F401
+
+    paths = (MODULE, sys.modules['bs4'].__path__[0])
+    tb = traceback.extract_stack()
+    previous = None
+    filename = None
+    lineno = None
+    for entry in tb:
+        if (PY35 and entry.filename.startswith(paths)) or (not PY35 and entry[0].startswith(paths)):
+            break
+        previous = entry
+    if previous:
+        filename = previous.filename if PY35 else previous[0]
+        lineno = previous.lineno if PY35 else previous[1]
+
+    warnings.warn_explicit(
+        "\nCSS selector pattern: {!r}\n".format(pattern) +
+        "    {}\n".format(message) +
+        "    This behavior is only allowed temporarily for Beautiful Soup's transition to Soup Sieve.\n" +
+        "    In order to confrom to the CSS spec, {}\n".format(recommend) +
+        "    It is strongly recommended the selector be altered to conform to the CSS spec " +
+        "as an exception will be raised for this case in the future.\n",
+        QuirksWarning,
+        filename,
+        lineno
     )
