14918082e2736a2103b83b66c0b2d933fd4ff161
h3llrais3r <pooh_beer_1@hotmail.com>
2020-10-15 09:26:46 +0200

    Fix test_display_logfile

--- tests/core/test_logger.py
+++ tests/core/test_logger.py
@@ -64,10 +64,10 @@ def test_display_logfile():
         line2 = u'traceback'
         line3 = u'2016-06-06 20:32:15,509 DEBUG    [MainThread :: __main__] System encoding: cp1252'
         line4 = u'2016-06-06 20:32:15,509 DEBUG    [MainThread :: __main__] Config version: 10'
-        lines = line1 + linesep + line2 + linesep + line3 + linesep + line4 + linesep
-        lines_info = line1 + linesep + line2 + linesep
-        lines_debug = line3 + linesep + line4 + linesep
-        lines_debug_reversed = line4 + linesep + line3 + linesep
+        lines = line1 + linesep + line2 + linesep + line3 + linesep + line4
+        lines_info = line1 + linesep + line2
+        lines_debug = line3 + linesep + line4
+        lines_debug_reversed = line4 + linesep + line3
         fd, autosubliminal.LOGFILE = tempfile.mkstemp(text=True)
         file = open(autosubliminal.LOGFILE, 'w')
         file.write(line1 + '\n' + line2 + '\n' + line3 + '\n' + line4 + '\n')
