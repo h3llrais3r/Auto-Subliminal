fc00b026df5dc4be0fe5fd0eadfad53da3450afc
h3llrais3r <pooh_beer_1@hotmail.com>
2020-11-02 17:42:02 +0100

    Install pushbullet.py version 0.12.0

--- lib/pushbullet/filetype.py
+++ lib/pushbullet/filetype.py
@@ -1,26 +1,25 @@
-def _magic_get_file_type(f, _):
-    file_type = magic.from_buffer(f.read(1024), mime=True)
-    f.seek(0)
-    return maybe_decode(file_type)
+
+try:
+    from magic import from_buffer as magic_from_buffer
+except ImportError:
+    import mimetypes
 
 
-def _guess_file_type(_, filename):
-    return mimetypes.guess_type(filename)[0]
+def get_file_type(file, filename):
+    try:
+        file_type = magic_from_buffer(file.read(1024), mime=True)
+        file.seek(0)
+        return maybe_decode(file_type)
+    except NameError:
+        return mimetypes.guess_type(filename)[0]
 
 
 # return str on python3.  Don't want to unconditionally
 # decode because that results in unicode on python2
 def maybe_decode(s):
-    if str == bytes:
-        return s.decode('utf-8')
-    else:
-        return s
+    try:
+        decoded = s.decode('utf-8')
+    except AttributeError as e:
+        decoded = s
+    return decoded
 
-
-try:
-    import magic
-except ImportError:
-    import mimetypes
-    get_file_type = _guess_file_type
-else:
-    get_file_type = _magic_get_file_type
