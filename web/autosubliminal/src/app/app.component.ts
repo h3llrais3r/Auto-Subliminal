86e6972080efb967c9f451395cd97a3235bf708d
h3llrais3r <pooh_beer_1@hotmail.com>
2021-01-08 21:50:36 +0100

    Enable system restart after settings update

--- web/autosubliminal/src/app/app.component.ts
+++ web/autosubliminal/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppSettingsService } from './app-settings.service';
+import { SystemEventService } from './core/services/system-event.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,10 @@ export class AppComponent {
 
   appSettingsLoaded = false;
 
-  constructor(private appSettingsService: AppSettingsService) {
+  constructor(private router: Router, private appSettingsService: AppSettingsService, private systemEventService: SystemEventService) {
     // Check if app settings are loaded
     this.appSettingsLoaded = this.appSettingsService.loaded();
+    // Subscribe on system restarted events
+    this.systemEventService.systemRestarted.subscribe(() => this.router.navigateByUrl('/system/restart?triggered=true'));
   }
 }
