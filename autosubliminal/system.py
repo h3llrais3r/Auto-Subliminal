5b0fa9cf5d163c41da25ff578ab33fce08014c78
h3llrais3r <pooh_beer_1@hotmail.com>
2021-03-03 18:44:24 +0100

    Show notifications after flush

--- autosubliminal/system.py
+++ autosubliminal/system.py
@@ -60,8 +60,8 @@ def flush_wanted_items():
         WantedItemsDb().flush_wanted_items()
         autosubliminal.WANTEDQUEUE = []
         release_wanted_queue_lock()
-        send_websocket_notification(
-            'Flushed wanted items database. Please launch \'Scan Disk\' from the \'System\' menu.')
+        send_websocket_notification('Flushed wanted items database.')
+        send_websocket_notification('Please launch \'Scan Disk\' from the \'System\' menu.')
     else:
         send_websocket_notification('Cannot flush wanted items database when wanted queue is in use!', severity='warn')
 
@@ -82,5 +82,6 @@ def flush_library():
         ShowDetailsDb().flush_shows(episodes=True, subtitles=True)
         MovieDetailsDb().flush_movies(subtitles=True)
         send_websocket_notification('Flushed library database.')
+        send_websocket_notification('Please launch \'Scan Library\' from the \'System\' menu.')
     else:
         send_websocket_notification('Cannot flush library database when library scanner is running!', severity='warn')
