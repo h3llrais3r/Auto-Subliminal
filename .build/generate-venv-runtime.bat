debc6ca704655696eccc3a5e4c8110fb29bcc069
h3llrais3r <pooh_beer_1@hotmail.com>
2021-03-17 19:13:40 +0100

    Add build bat files for generating venvs

---
+++ .build/generate-venv-runtime.bat
@@ -0,0 +1,25 @@
+echo off
+echo.
+echo Removing old venv
+echo.
+cd ../.venv
+rmdir runtime /s
+cd ..
+echo.
+echo Creating new venv
+python -m venv .venv/runtime
+echo.
+echo Activating venv
+call .venv/runtime/scripts/activate
+echo.
+echo Installing required windows dependencies
+echo.
+python -m pip install pywin32
+set /p deactivate=Deactivate venv (y/[N]):
+if /I "%deactivate%"=="y" goto yes
+goto no
+:yes
+echo. 	
+echo Deactivating venv
+call .venv/runtime/scripts/deactivate
+:no
