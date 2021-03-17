3a55ec86f2df93d4d510990225a6e0ac796273c0
h3llrais3r <pooh_beer_1@hotmail.com>
2021-03-17 19:13:40 +0100

    Add build bat files for generating venvs

---
+++ .build/generate-venv-dev.bat
@@ -0,0 +1,27 @@
+echo off
+echo.
+echo Removing old venv
+echo.
+cd ../.venv
+rmdir dev /s
+cd ..
+echo.
+echo Creating new venv
+python -m venv .venv/dev
+echo.
+echo Activating venv
+call .venv/dev/scripts/activate
+echo.
+echo Installing dev dependencies
+echo.
+python -m pip install -r requirements-dev.txt
+rem if "keep-active" is passed as argument, bypass the deactivate check
+if "%1%"=="keep-active" goto no
+set /p deactivate=Deactivate venv (y/[N]):
+if /I "%deactivate%"=="y" goto yes
+goto no
+:yes
+echo.
+echo Deactivating venv
+call .venv/dev/scripts/deactivate
+:no
