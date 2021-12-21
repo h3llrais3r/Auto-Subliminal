77229f1517d18bebce2bac58e842a21bde372e54
h3llrais3r <pooh_beer_1@hotmail.com>
2021-12-21 21:02:23 +0100

    Rework app-settings.service
    Use APP_INITIALIZER with observable

--- web/autosubliminal/src/app/app-settings.service.ts
+++ web/autosubliminal/src/app/app-settings.service.ts
@@ -1,8 +1,8 @@
 import { PlatformLocation } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { AntiCaptchaProvider } from './shared/models/captcha';
 import { Language } from './shared/models/language';
 import { EpisodeScores, MovieScores } from './shared/models/score';
@@ -50,12 +50,8 @@ class AppSettings {
 
 export let appSettings = new AppSettings();
 
-export function appSettingsServiceFactory(appSettingsService: AppSettingsService): () => Promise<AppSettings> {
-  // A lambda is required here, otherwise `this` won't work inside AppSettingsService::load
-  return async () => {
-    // Wait for load to finish to be sure that the app is not bootstrapped before this is loaded (IE/Edge fix)
-    return await appSettingsService.load();
-  };
+export function appSettingsServiceFactory(appSettingsService: AppSettingsService): () => Observable<AppSettings> {
+  return () => appSettingsService.load();
 }
 
 @Injectable({
@@ -75,32 +71,26 @@ export class AppSettingsService {
     console.log(`Application web root: ${this.webRoot}`);
   }
 
-  // The return value (Promise) of this method is used as an APP_INITIALIZER, so the application's initialization will not complete until the Promise resolves.
-  public async load(): Promise<AppSettings> {
+  public load(): Observable<AppSettings> {
     if (this.configLoaded) {
-      // Return previously loaded config
-      return of(appSettings).toPromise();
+      // Return previously loaded settings
+      return of(appSettings);
     } else {
-      // Use manual resolve, reject to be sure to not bootstrap the application in case of an error!
-      return new Promise((resolve, reject) => {
-        const settingsObservable = this.httpClient.get(`${this.webRoot}/api/system/settings`).pipe(map((settings) => settings as AppSettings));
-        settingsObservable.subscribe(
-          (settings) => {
-            appSettings.fromSettings(settings);
-            this.configLoaded = true;
-            console.log('Application settings loaded');
-            console.log(`Application version: ${appSettings.appVersion}`);
-            console.log(`Application PID: ${appSettings.appProcessId}`);
-            console.log(`Developer mode: ${appSettings.developerMode}`);
-            resolve(appSettings);
-          },
-          (error) => {
-            // Use reject if you want to break the boostrapping of the app!
-            // reject(`Error while retrieving application settings: ${JSON.stringify(error)}`);
-            console.error(`Error while retrieving application settings: ${JSON.stringify(error)}`);
-            resolve(null);
-          });
-      });
+      // Load settings
+      return this.httpClient.get(`${this.webRoot}/api/system/settings`)
+        .pipe(map((settings) => {
+          appSettings.fromSettings(settings);
+          this.configLoaded = true;
+          console.log('Application settings loaded');
+          console.log(`Application version: ${appSettings.appVersion}`);
+          console.log(`Application PID: ${appSettings.appProcessId}`);
+          console.log(`Developer mode: ${appSettings.developerMode}`);
+          return appSettings;
+        }))
+        .pipe(catchError((error) => {
+          console.error(`Error while retrieving application settings`);
+          return throwError(error); // rethrow error to be sure that app initialization stops
+        }));
     }
   }
 
