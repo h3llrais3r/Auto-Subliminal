045c6f18bd145748fb0432633bb57980dbe568b0
h3llrais3r <h3llrais3r.github@gmail.com>
2023-03-17 18:17:54 +0100

    Update application.py

--- autosubliminal/application.py
+++ autosubliminal/application.py
@@ -265,13 +265,18 @@ def stop(exit=True):
 
     # Stop scheduled threads
     log.info('Stopping threads')
-    autosubliminal.SCANDISK.stop()
-    autosubliminal.CHECKSUB.stop()
-    autosubliminal.CHECKVERSION.stop()
-    autosubliminal.SCANLIBRARY.stop()
+    if autosubliminal.SCANDISK:
+        autosubliminal.SCANDISK.stop()
+    if autosubliminal.CHECKSUB:
+        autosubliminal.CHECKSUB.stop()
+    if autosubliminal.CHECKVERSION:
+        autosubliminal.CHECKVERSION.stop()
+    if autosubliminal.SCANLIBRARY:
+        autosubliminal.SCANLIBRARY.stop()
 
     # Stop permanent threads
-    autosubliminal.WEBSOCKETBROADCASTER.stop()
+    if autosubliminal.WEBSOCKETBROADCASTER:
+        autosubliminal.WEBSOCKETBROADCASTER.stop()
 
     if exit:
         _shutdown()
