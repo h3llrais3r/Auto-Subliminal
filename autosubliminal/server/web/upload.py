c9b15d0b96d3d0d2b781895b42172bdb134126b7
h3llrais3r <h3llrais3r.github@gmail.com>
2025-06-02 18:13:30 +0200

    Fix tmp file upload api

--- autosubliminal/server/web/upload.py
+++ autosubliminal/server/web/upload.py
@@ -16,9 +16,9 @@ class Upload(object):
     """ Upload path."""
 
     @cherrypy.expose(alias='tmp')
-    def tmp(self, file_upload: FileUpload) -> str:
+    def tmp(self, file: FileUpload) -> str:
         # Get tmp file path
-        tmp_file = os.path.abspath(os.path.join(tempfile.gettempdir(), file_upload.filename))
+        tmp_file = os.path.abspath(os.path.join(tempfile.gettempdir(), file.filename))
 
         # Remove any previous tmp file with same name
         if os.path.exists(tmp_file):
@@ -27,7 +27,7 @@ class Upload(object):
         # Write tmp file
         with open(tmp_file, mode='wb') as handler:
             while True:
-                data = file_upload.file.read(8192)
+                data = file.file.read(8192)
                 if not data:
                     break
                 handler.write(data)
